b1fa1dbadfae8318a91b8b2f2634418ef2a9d5f6
eFacorro <facorroesteiro@gmail.com>
2024-02-12 21:25:23 +0100

    enviando al ws

--- front/src/componentes/enviar.jsx
+++ front/src/componentes/enviar.jsx
@@ -1,10 +1,15 @@
-import React from "react";
+import React, {useState} from "react";
 import { wsSend } from "../js/helpers";
 
 export default function Enviar(){
+  let [msg, setMsg] = useState("");
+  const enviar = () => {
+    wsSend(msg);
+    setMsg("");
+  }
   return (
     <div>
-      <input type="text" /> <button onClick={()=>{wsSend("boton")}}>Enviar</button>
+      <input type="text" value={msg} onChange={(e) => {setMsg(e.target.value);}}/> <button onClick={enviar}>Enviar</button>
     </div>
   )
 }
