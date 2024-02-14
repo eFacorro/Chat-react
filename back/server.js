1ae4067ee3a8bf64275f2b68c3e923316f4aaed0
eFacorro <facorroesteiro@gmail.com>
2024-02-14 20:03:16 +0100

    empezando con el back

--- back/server.js
+++ back/server.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const express = require("express");
 const path = require("path");
 const cors = require("cors");
+const { test } = require('./funciones/funciones');
 
 const app = express();
 app.use(express.json());
@@ -11,6 +12,7 @@ app.use(cors());
 // const carpetaStatic = path.join(__dirname, "static");
 
 // app.use(express.static(carpetaStatic));
+app.get("/test",test);
 
 app.listen(4000, function () {
   console.log("Server running");
